c0cfc3fa21a614801b8dd423cd4493925b7709cc
Marc-Olivier-CAS <marcolivier_cas@yahoo.fr>
2023-05-16 13:53:57 +0200

    added spring project

---
+++ maximilienmarco/src/main/kotlin/com/example/maximilienmarco/MaximilienmarcoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.maximilienmarco
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MaximilienmarcoApplication
+
+fun main(args: Array<String>) {
+	runApplication<MaximilienmarcoApplication>(*args)
+}
---
+++ maximilienmarco/src/test/kotlin/com/example/maximilienmarco/MaximilienmarcoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.maximilienmarco
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MaximilienmarcoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
